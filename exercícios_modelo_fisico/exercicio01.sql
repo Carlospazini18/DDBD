dd323a34ab3d5ddfedbaa31821e9c524e2b5ee53
Carlos Henrique Santos Pazini <97403238+Carlospazini18@users.noreply.github.com>
2024-04-25 20:34:11 -0300

    aprendendo a inserir dados em um banco de dados

--- exercícios_modelo_fisico/exercicio01.sql
+++ exercícios_modelo_fisico/exercicio01.sql
@@ -40,3 +40,30 @@ constraint cpf_paciente_consulta foreign key(cpf_paciente_consulta)
 references paciente(cpf)
 );
 
+-- INSERINDO VALORES NAS TABELAS 
+
+insert into paciente(cpf, nome, data_nasc, telefone, email)
+values("111.111.111-22", "ΑΝΟΝ ΝΟΝΟΝΟΝ", "1970-12-24","+5527999991111","ANON@ANON.COM"),
+("222.222.111-22","NONA NANANAN", "1990-05-01","+5527999992222", "NONA@ANON.COM"),
+("333.333.111-22", "ΝΟΝΝΟ ΝΟΝΟΝΟΝ", "1985-02-28","+5527999993333","ΝΟΜNO@ANON.COM");
+
+insert into departamento(cod_departamento, nome_departamento) 
+values(001, "clínico geral"), (002, "gastroentereologista"), (003, "cardiologista");
+
+insert into medico(CRM, UF, nome, email, telefone, medico_id_departamento) values 
+(1023, "ES", "JESUINO TAMANDARE", "JESUTANAN@MEDICO.NET","+5527989898766", 001),
+(1432, "RS", "BENTO GONÇALVES", "BENTOGON@MEDICO.NET", "45527998983766", 002),
+(2123, "CE", "MARIA BONITA", "MARIABONITA@MEDICO.NET", " +5527984543543", 003);
+
+insert into consultas (id_consulta, anamnese, date_consulta, hora_consul, cpf_paciente_consulta,
+ CRM_medico_consulta, UF_medico_consulta) values 
+(001, "PACIENTE INFARTOU", "2024-04-10", "15:41:00", "111.111.111-22", 1023, "ES"),
+(002,"PACIENTE ESTÁ COM FEBRE","2024-04-10", "12:10:00", "333.333.111-22", 1432, "RS"),
+(003, "PACIENTE INFARTOU", "2024-04-10", "15:41:00", "222.222.111-22", 2123, "CE");
+
+-- SELECIONANDO AS TABELAS PARA MOSTRAREM OS DADOS
+
+-- select * from consultas;
+-- select * from medico;
+-- select * from departamento;
+-- select * from paciente;
