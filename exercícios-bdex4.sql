ce1a5df96a8630deda1b18384116eb2c334498fa
Carlos Henrique Santos Pazini <97403238+Carlospazini18@users.noreply.github.com>
2024-06-07 23:56:17 -0300

    Update exercícios-[bdex4.sql

--- exercícios-bdex4.sql
+++ exercícios-bdex4.sql
@@ -29,7 +29,7 @@ JOIN
 
 #6. Mostrar os nomes dos fornecedores que têm produtos no armazém de "São Paulo".
 SELECT
-	forn.nome_for
+	forn.nome_for, arm.cidade_armazem
 FROM
 	fornecedores forn
 JOIN
@@ -39,19 +39,51 @@ JOIN
 JOIN
 	produtos_armazens pa ON pa.produto = prod.id_produto
 JOIN
-	armazens arm ON arm.id_armazem = pa.produto;
+	armazens arm ON arm.id_armazem = pa.produto
+WHERE 
+	arm.cidade_armazem = 'São Paulo';
 
 #7. Listar todos os armazéns, ordenados pelo nome do armazém de A a Z.
+SELECT nome_armazem FROM armazens ORDER BY nome_armazem;
 
 #8. Mostrar os nomes dos produtos que têm a palavra "Premium" na descrição.
+SELECT  nome_pro, descricao FROM  produtos WHERE descricao like '%Premium%';
 
 #9. Exibir os detalhes dos produtos: ID do produto, nome, descrição, preço e nome dos fornecedores.
+SELECT 
+	pd.id_produto, pd.nome_pro, pd.descricao, pd.preco, forn.nome_for
+FROM
+	produtos pd 
+JOIN
+	produtos_fornecedores pf ON pf.produto = pd.id_produto
+JOIN
+	fornecedores forn ON forn.id_fornecedor = pf.fornecedor;
 
 #10. Para cada pedido de compra, mostrar o ID do pedido e a quantidade total de produtos solicitados.
+SELECT 
+	pc.id_pedido, 
+    count(pc.produto) AS Qtde 
+FROM
+	pedidos_compra pc
+GROUP BY 
+	pc.id_pedido
+;
 
 #11. Contar quantos produtos cada fornecedor oferece.
+SELECT 
+	forn.nome_for, count(pd.nome_pro) AS 'Qntd de produtos'
+FROM
+	fornecedores forn
+INNER JOIN
+	produtos_fornecedores pf ON  pf.fornecedor = forn.id_fornecedor
+INNER JOIN 
+	produtos pd ON pd.id_produto = pf.produto
+GROUP BY 
+	forn.nome_for
+;
 
 #12. Listar os nomes dos produtos e a quantidade disponível no armazém "Rio de Janeiro".
+SELECT pd.nome_prod,
 
 #13. Mostrar o nome dos fornecedores e a data e hora do último pedido de compra que eles receberam.
 
